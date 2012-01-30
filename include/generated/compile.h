fe0ec4aa8a113284b8f98441167a96086367adae
Bdaman80 <bdaman80@gmail.com>
2012-01-29 18:01:21 -0600

    Sync

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 PREEMPT Sun Jan 22 07:34:52 CST 2012"
-#define LINUX_COMPILE_TIME "07:34:52"
+#define UTS_VERSION "#11 PREEMPT Sun Jan 29 17:07:11 CST 2012"
+#define LINUX_COMPILE_TIME "17:07:11"
 #define LINUX_COMPILE_BY "bdaman"
 #define LINUX_COMPILE_HOST "Height03"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.10-BD-Ace_2012-1-22_WCHRage-00040-g214d429-dirty"
+#define UTS_RELEASE "2.6.35.10-BD-Ace_2012-1-29_WCHRage-00046-ge54070c-dirty"
