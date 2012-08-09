7870ab5d7c7d094a06ce6cbdf02d10c8fa374f99
Bdaman80 <bdaman80@gmail.com>
2012-08-08 20:47:46 -0500

    Add build script

---
+++ envset-linaro.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+# This script is to set enviromental variables for android kernel building
+#To use Linaro 4.7 tool chain
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+export PATH=$PATH:/1TB/DroidBuilder/sources/toolchains/linaro-4.7-android-toolchain-eabi/bin
+
+
+
+
